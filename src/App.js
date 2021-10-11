19dad664a093a7571ef9418830f344cd00a1e29e
Kelvin <kelvinkamau387@gmail.com>
2021-10-11 19:03:31 +0300

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui container comments">
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Kelvin"
+          timeAgo={"Today at 3:00PM"}
+          avatar={faker.image.avatar()}
+          comment={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Joshua"
+          timeAgo="Today at 6:00PM"
+          avatar={faker.image.avatar()}
+          comment={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Mary"
+          timeAgo="Today at 12:00PM"
+          avatar={faker.image.avatar()}
+          comment={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
     </div>
   );
-}
+};
 
 export default App;
